49a303da97cbaedef1e5d0c11e60d5156bf8ef83
samikshyasu <163755362+samikshyasu@users.noreply.github.com>
2024-03-19 20:31:12 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -12,7 +12,7 @@ public class Login extends JFrame implements ActionListener{
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
         
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("login.jpg"));
         JLabel image = new JLabel(i1);
         image.setBounds(0,0,600,500);
         add(image);
